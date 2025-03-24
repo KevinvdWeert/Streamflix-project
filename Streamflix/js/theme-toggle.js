b508406e766c0ec8a7c2f057da3d89b00012fa85
KevinvdWeert <Ke.vandeweert@gmail.com>
2025-03-24 14:12:30 +0100

    added login page, added js script.

--- Streamflix/js/theme-toggle.js
+++ Streamflix/js/theme-toggle.js
@@ -1,19 +1,5 @@
 $(document).ready(function() {
     $('#theme-toggle').click(function() {
         $('body, header, footer, nav').toggleClass('light-theme');
-        $('body, header, footer, nav').css('transition', 'background-color 0.5s, color 0.5s');
-        
-        // Toggle between black and white themes
-        if ($('body').hasClass('light-theme')) {
-            $('body, header, footer, nav').css({
-                'background-color': 'white',
-                'color': 'black'
-            });
-        } else {
-            $('body, header, footer, nav').css({
-                'background-color': 'black',
-                'color': 'white'
-            });
-        }
     });
 });
